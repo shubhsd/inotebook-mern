53d2d70e6f116364747e13bce5580c3e457608c0
shubhsd <shubhamsd53@gmail.com>
2021-09-12 11:46:50 +0530

    backend folder - models and routes created

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,13 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// Available routes-------->>>>>>>>>>>>
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
