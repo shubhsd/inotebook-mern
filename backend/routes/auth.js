581eaa1921103a9116c2473fcfefc30ada787521
shubhsd <shubhamsd53@gmail.com>
2021-09-13 10:01:05 +0530

    backend folder --- bcrypt / hash and salt added for password storage mechanism in auth.js file

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 // Create a user : using POST "/api/auth/createUser" . No login required
@@ -11,7 +11,7 @@ router.post('/createUser', [
     body('password', 'Password must be atleast 8 characters').isLength({ min: 8 }),
     body('email', 'Enter a valid email').isEmail(),
 ], async(req, res) => {
-    // Making new user 
+    // Returns bad requests if there are errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -22,10 +22,15 @@ router.post('/createUser', [
         if (user) {
             return res.status(400).json({ error: 'User with this email already exists' });
         }
+
+        const salt = await bcrypt.genSalt(10);
+        // Await added in both lines because these both methods are promise so until and unless these two promises are resolved we can't move further otherwise it will give error.
+        const secPassword = await bcrypt.hash(req.body.password, salt);
+        // Create new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPassword,
         })
         res.json(user);
     } catch (error) {
