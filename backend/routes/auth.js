6c2eb2eb2b36eab5f8f8c1c9fd88ab0a6d617e30
shubhsd <shubhamsd53@gmail.com>
2021-09-13 11:30:42 +0530

    backend folder -----  login endpoint created  + express validator for multiple validations on same field + jwt compare method used

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, checkSchema, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -10,7 +10,19 @@ const JWT_SECRET = 'Shubhamisagoodb$oy';
 
 // Create a user : using POST "/api/auth/createUser" . No login required
 
-router.post('/createUser', [
+const registrationSchema = {
+    password: {
+        isStrongPassword: {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1
+        },
+        errorMessage: "Password must be greater than 8 and contain at least one uppercase letter, one lowercase letter, one number and special character ",
+    },
+}
+
+router.post('/createUser', checkSchema(registrationSchema), [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('password', 'Password must be atleast 8 characters').isLength({ min: 8 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -52,8 +64,53 @@ router.post('/createUser', [
         // res.json({ authToken: authToken });
 
     } catch (error) {
-        console.log(error.message, '---------->>>>>>Error from auth.js fileF');
-        res.status(500).send('Some error occured');
+        console.log(error.message, '---------->>>>>>Error from auth.js file --- create user api');
+        res.status(500).send('Internal server error');
+    }
+});
+
+// Authenticate a user : using POST "/api/auth/login" . No login required
+
+
+
+router.post('/login', [
+    body('email', 'Email is required').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async(req, res) => {
+    // Returns bad requests if there are errors 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        // Check whether user with same email already exists or not
+        // User - capital u this is our model.
+        let user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid credentials' });
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        // Compare is asynchronous method - checked after hovering so we will use await here and will return true or false - we got to know this on hovering it said it return promise<boolean>.
+        // First password passed as argument above is what you user have entered- destructured above and 2nd argument i.e user.password is the one that we found using findOne query from the database.
+
+        if (!passwordCompare) {
+            return res.status(400).json({ error: 'Invalid credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(payload, JWT_SECRET);
+        res.json({ authToken });
+
+    } catch (error) {
+        console.log(error.message, '---------->>>>>>Error from auth.js file ---- login api');
+        res.status(500).send('Internal server error');
     }
 });
 
