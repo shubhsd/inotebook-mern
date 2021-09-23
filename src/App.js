332035b1faedd90b4c14103a40da096d509ef3b4
shubhsd <shubhamsd53@gmail.com>
2021-09-23 10:08:10 +0530

    Frontent folder ------ Use location hook

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Switch>
-            <Route exact path="/"><Home /> </Route>
-            <Route exact path="/about"><About /></Route>
-          </Switch>
+          <div className="container">
+            <Switch>
+              <Route exact path="/"><Home /> </Route>
+              <Route exact path="/about"><About /></Route>
+            </Switch>
+          </div>
         </Router>
       </NoteState>
     </>
